639975b49262389eba797bb6a8f7a13773594332
stop-pattern <railwaysandstationfan+Git@gmail.com>
2019-01-19 01:15:48 +0900

    [fix] 基板Lチカコードのミスを修正

--- code/test_LED/test_LED.ino
+++ code/test_LED/test_LED.ino
@@ -1,13 +1,28 @@
-#define IO0 0
-#define LED 22
+bool stat;
+double LED;
+String light;
 
 void setup() {
-  pinMode(IO0, INPUT);
-  pinMode(LED, OUTPUT);
+  pinMode(0, INPUT);
+  pinMode(22, OUTPUT);
+  Serial.begin(74880);  //起動メッセージ文字化け防止
+  while (!Serial);
+  Serial.println("");
+  Serial.println("Connection completed");
 }
 
 void loop() {
-  if (IO0 == HIGH) {
-    digitalWrite( LED, HIGH);
+  stat = false;
+  if (analogRead(0) < 2000) {
+    if(LED != LOW)stat = true;
+    LED = LOW;
+    light = "ON";
+  } else {
+    if(LED != HIGH)stat = true;
+    LED = HIGH;
+    light = "OFF";
   }
+  digitalWrite(22, LED);
+  if(stat == true) Serial.println(light);
+  delay(1);
 }
