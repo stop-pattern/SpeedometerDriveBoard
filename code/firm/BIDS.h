07e2ff6beaeff3b719fd582e8e2a080bfb0f1b1d
stop-pattern <railwaysandstationfan+Git@gmail.com>
2019-02-23 19:13:05 +0900

    update firmware

--- code/firm/BIDS.h
+++ code/firm/BIDS.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 
 namespace BIDS {
@@ -7,13 +6,3 @@ float DataGet(String, String, int);
 float DataGet(String, int);
 int VersionCheck(int);
 };
-=======
-#pragma once
-
-namespace BIDS {
-float SerialGet(String);
-float DataGet(String, String, int);
-float DataGet(String, int);
-int VersionCheck(int);
-};
->>>>>>> acf3d5ac075861e1d6ade95787c2d3ec12d00e4b
