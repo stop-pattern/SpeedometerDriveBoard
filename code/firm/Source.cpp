2abd46fcb9340708d089d1776cbe6b9a4107e420
stop-pattern <railwaysandstationfan+Git@gmail.com>
2019-02-24 22:58:41 +0900

    bell改良&通信回数削減

--- code/firm/Source.cpp
+++ code/firm/Source.cpp
@@ -36,12 +36,8 @@ void ShiftOut(bool senddata[]) {
 
 void GetData(void) {
   SetNeedle();
-  SetSound();
-  if (cnt >= 5) {
-    SetLamp();
-    cnt = 0;
-  }
-  cnt++;
+  //SetSound();
+  SetLamp();
 }
 
 void SetNeedle(void) {
@@ -71,74 +67,95 @@ void SetSound(void) {
 }
 
 void SetLamp(void) {
-  for (int i = 0; i < 25 ; i++) {
-    indicator[i] = BIDS::DataGet("I", "P", i + 102);  //ATC指示値
-    if (indicator[i]) {
-      for (i = i + 1; i < 25 ; i++) {
-        indicator[i] = 0;
-      }
-      i = 100;
-    }
-  }
+  int param;
 
-  R = BIDS::DataGet("I", "P", 131);
-  G = BIDS::DataGet("I", "P", 132);
-  notice = BIDS::DataGet("I", "P", 133);
-  P = BIDS::DataGet("I", "P", 134);
   X = BIDS::DataGet("I", "P", 101);
+
+  if (X != 0) {
+    R = BIDS::DataGet("I", "P", 131);
+    G = BIDS::DataGet("I", "P", 132);
+    notice = BIDS::DataGet("I", "P", 133);
+    P = BIDS::DataGet("I", "P", 134);
+  } else {
+    R = false;
+    G = false;
+    notice = false;
+    P = false;
+  }
   BL = BIDS::DataGet("I", "P", 161);
   L15 = BIDS::DataGet("I", "P", 42);
   L60 = BIDS::DataGet("I", "P", 43);
   ATS = BIDS::DataGet("I", "P", 44);
-}
 
-void SetData(void) {
-  for (int i = 0; i < 25; i++) {
-    shiftregister[i] = indicator[i];
+  for (int i = 0; i < 25 ; i++) {
+    if (indicator[i] == true) {
+      param = i;
+      break;
+    }
   }
-  //shink
-  shiftregister[25] = R;
-  shiftregister[26] = G;
-  shiftregister[27] = notice;
-  shiftregister[28] = P;
-  shiftregister[29] = X;
-  shiftregister[30] = 0;
-  shiftregister[31] = 0;
-  shiftregister[32] = 0;
-  shiftregister[33] = 0;
-  shiftregister[34] = 0;
-  shiftregister[35] = 0;
-  shiftregister[36] = 0;
-  shiftregister[37] = 0;
-  shiftregister[38] = 0;
-  shiftregister[39] = 0;
-
-  //source
-  shiftregister[40] = L15;
-  shiftregister[41] = L60;
-  shiftregister[42] = ATS;
-  shiftregister[43] = bell;
-  shiftregister[44] = 0;//BL;
-  shiftregister[45] = 0;
-  shiftregister[46] = 0;
-  shiftregister[47] = 0;
-
-  ShiftOut(shiftregister);
-  bell = false;
-}
-
-
-void demo(void) {
-  /*
-    double B = 255, R = 255;
-    while (true) {
-      if(analogRead(IO0) == HIGH) break;
-      for (int i = 0 ; i < 8; i++) {
-      shiftOut(SER, SCK, MSBFIRST, 0b11111111);
+  if (BIDS::DataGet("I", "P", param + 102) == true) {
+    bell = false;
+  } else {
+    for (int i = 0; i < 25 ; i++) {
+      indicator[i] = BIDS::DataGet("I", "P", i + 102);  //ATC指示値
+      if (indicator[i]) {
+        for (i = i + 1; i < 25 ; i++) {
+          indicator[i] = 0;
+        }
+        i = 100;
       }
-      digitalWrite(RCK, LOW);
-      digitalWrite(RCK, HIGH);
-      dacWrite(DAC1,B);
-      dacWrite(DAC2,R);
-    }*/
+    }
+    bell = true;
+  }
 }
+
+  void SetData(void) {
+    for (int i = 0; i < 25; i++) {
+      shiftregister[i] = indicator[i];
+    }
+    //shink
+    shiftregister[25] = R;
+    shiftregister[26] = G;
+    shiftregister[27] = notice;
+    shiftregister[28] = P;
+    shiftregister[29] = X;
+    shiftregister[30] = 0;
+    shiftregister[31] = 0;
+    shiftregister[32] = 0;
+    shiftregister[33] = 0;
+    shiftregister[34] = 0;
+    shiftregister[35] = 0;
+    shiftregister[36] = 0;
+    shiftregister[37] = 0;
+    shiftregister[38] = 0;
+    shiftregister[39] = 0;
+
+    //source
+    shiftregister[40] = L15;
+    shiftregister[41] = L60;
+    shiftregister[42] = ATS;
+    shiftregister[43] = bell;
+    shiftregister[44] = 0;//BL;
+    shiftregister[45] = 0;
+    shiftregister[46] = 0;
+    shiftregister[47] = 0;
+
+    ShiftOut(shiftregister);
+    bell = false;
+  }
+
+
+  void demo(void) {
+    /*
+      double B = 255, R = 255;
+      while (true) {
+        if(analogRead(IO0) == HIGH) break;
+        for (int i = 0 ; i < 8; i++) {
+        shiftOut(SER, SCK, MSBFIRST, 0b11111111);
+        }
+        digitalWrite(RCK, LOW);
+        digitalWrite(RCK, HIGH);
+        dacWrite(DAC1,B);
+        dacWrite(DAC2,R);
+      }*/
+  }
